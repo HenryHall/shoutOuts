b38d3e5b21a430e856dc21d2c5bb910289952897
HenryHall <mnboogie@yahoo.com>
2017-07-10 15:17:26 -0500

    env test

--- server/modules/connection.js
+++ server/modules/connection.js
@@ -1,3 +1,12 @@
-var connectionString = 'postgres://localhost:5432/shoutouts';
+
+var pg = require('pg');
+var connectionString = '';
+if(process.env.DATABASE_URL !== undefined) {
+ connectionString = process.env.DATABASE_URL;
+ pg.defaults.ssl = true;
+} else {
+ connectionString = 'postgres://localhost:5432/shoutouts';
+}
+console.log("connectionString set to: ", connectionString);
 
 module.exports = connectionString;
